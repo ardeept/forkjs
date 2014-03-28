69715eb5b28ebb18a269648253745f6af192b1e1
Ardee Yanto <ardee@promotexter.com>
2014-03-29 00:21:46 +0800

    FIXES

---
+++ app-test.js
@@ -0,0 +1,99 @@
+/** 
+
+   SAMPLE APP 
+
+   This app will do nothing but to output "Hello world" into the stream until it reaches the max count.
+
+**/
+
+
+var util            = require('util');
+var EventEmitter    = require('events').EventEmitter;
+
+var Echo = function(c)
+{
+   var self = this;
+
+   self.config = c;
+
+   self.echoed = 0;
+
+   
+
+   self.init = function()
+   {
+      self.id     = self.config.id;
+      
+      self.max    = Math.floor((Math.random()*10)+1);;
+   
+      self.process = false;
+      self.status  = 'ready';
+
+      self.emit('status','ready');
+   }
+
+   self.run = function()
+   {
+      if(self.status == 'running')
+      {
+         // already running
+         self.emit('status','already running');
+      }
+      else
+      {
+         self.running = setInterval(self.echo_console, 1000);      
+
+         self.status = 'running';
+
+         self.emit('status','running');
+      }
+   }
+
+   self.echo_console = function()
+   {
+      if(self.status == 'running')
+      {
+         self.echoed++
+
+         // console.log(self.echoed, 'Hello world');
+
+         self.emit('status','Sent ' + self.echoed + " of " + self.max); 
+
+         // is this the last one?
+         if(self.echoed == self.max)
+         {
+             self.done();
+         }
+      }
+      else
+      {
+         self.emit('status','Not running'); 
+      }
+   }
+
+   self.done = function()
+   {
+      clearInterval(self.running);
+      self.emit('status','Job completed.');
+
+      process.exit(0);
+   }
+
+   self.stop = function()
+   {
+      clearInterval(self.running);
+
+      self.status  = 'stopped';
+
+      self.emit('status','stopped');
+   }
+
+   self.resume = function()
+   {
+      self.run();
+   }
+}
+
+util.inherits(Echo, EventEmitter);
+
+module.exports = Echo;
--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 var config = {};
 
-config.fork_server_port 		= 8081;
-config.process_path 			= "/processes/";
+config.http_server_port 		= 8081;
 config.max_instances			= 4;
-
+config.driver					= 'driver.js';
+config.app_name					= 'ECHO';
 
 
 module.exports 					= config;
